852eeee13949ae9579224ac31a673d75a8d90645
DrManganese <DrManganese@users.noreply.github.com>
2023-10-29 10:55:01 +0100

    Use custom markdown-preview fork and override stylesheet

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,9 +1,10 @@
 return {
-  '0x00-ketsu/markdown-preview.nvim',
+  --dir = '/Users/mathias.de.jong/MiscProjects/markdown-preview.nvim',
+  'DrManganese/markdown-preview.nvim',
   ft = { 'md', 'markdown', 'mkd', 'mkdn', 'mdwn', 'mdown', 'mdtxt', 'mdtext', 'rmd', 'wiki' },
   config = function()
     require('markdown-preview').setup {
-      glow = { exec_path = '/usr/local/bin/glow' }
+      glow = { exec_path = '/usr/local/bin/glow', style = '~/.config/nvim/glamour_macchiato.json' }
     }
     vim.keymap.set('n', '<Leader>m', require('markdown-preview').toggle, { desc = 'Toggle [m]arkdown preview' })
   end
